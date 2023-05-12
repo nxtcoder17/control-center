11387ed39f7941a0f7f76f6100096d6e85db3447
nxtcoder17 <nxtcoder17@gmail.com>
2023-05-13 02:53:31 +0530

    custom indexes have been added to tabs

--- src/components/tab.jsx
+++ src/components/tab.jsx
@@ -10,24 +10,20 @@ export const Tab = (props) => {
     }
   })
 
-  // createEffect(() => {
-  //   logger.info(props.matches, "matches")
-  //   const x = props.matches?.filter(m => m.key === "title").indices.map(midx => props.tabInfo.title.slice(midx[0], midx[1]))
-  //   logger.info(x, "tab title matches")
-  // }, "")
-
-  return <div class="flex flex-row gap-3 tracking-wide text-gray-700 rounded-md px-2 py-0.5 dark:hover:bg-slate-700 cursor-pointer transition-all"
+  return <div class="flex flex-row gap-3 tracking-wide text-gray-700 rounded-md px-2 py-1 dark:hover:bg-slate-700 cursor-pointer transition-all items-center"
     classList={{
       "bg-slate-200 dark:bg-slate-900 dark:text-slate-300": props.isSelected,
       "bg-slate-50 dark:bg-slate-700 dark:text-slate-400": !props.isSelected,
     }}
     ref={ref} onClick={() => props.onClick()}>
-    <div class="flex-initial w-5 truncate text-right">{props.index}</div>
-    <div class="w-5 h-5">
-      <img src={props.tabInfo.favIconUrl} />
+    <div class="flex flex-row gap-3 items-center">
+      <div class="flex-initial w-5 truncate text-right text-lg">{props.index}</div>
+      <div class="w-5 h-5 flex items-center justify-center">
+        <img src={props.tabInfo.favIconUrl} />
+      </div>
     </div>
-    <div class="flex-initial w-2/3 truncate">{props.tabInfo.title}</div>
-    <div class="flex-initial w-1/3 truncate">{props.tabInfo.url}</div>
+    <div class="flex-initial text-lg w-2/3 truncate">{props.tabInfo.title}</div>
+    <div class="flex-initial text-lg w-1/3 truncate">{props.tabInfo.url}</div>
     <div class="">{props.tabInfo.pinned ? <TbPinned class="w-5 h-5" /> : <div class='w-5 h-5' />}</div>
     <div class="">{props.tabInfo.mutedInfo?.muted ? < TbVolumeOff class="w-5 h-5" /> : <div class='w-5 h-5' />}</div>
   </div >
