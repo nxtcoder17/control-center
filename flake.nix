054e08e2de20fb9d4bc1b4fba3455339a6d51503
nxtcoder17 <nxtcoder17@gmail.com>
2023-12-18 20:23:12 +0530

    feat(NIX flake): adds nix flake for development

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "your dev environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          # hardeningDisable = [ "all" ];
+
+          buildInputs = with pkgs; [
+            # cli tools
+            go-task
+            zip
+            jq
+
+            # source version control
+            git
+            pre-commit
+
+            # programming tools
+            nodejs-slim_21
+            nodePackages.pnpm
+            nodePackages.npm
+
+            # build tools
+          ];
+
+          shellHook = ''
+            echo "using nix flake"
+          '';
+        };
+      }
+    );
+}
+
+
