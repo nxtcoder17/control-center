bed59d411529d6f2bc33150ab41a68209005ffef
nxtcoder17 <nxtcoder17@gmail.com>
2025-06-25 01:25:48 +0530

    feat: removes youtube shorts from search page
    - also updates flakes, and runfile

--- src/content-scripts/youtube.com/removes-youtube-shorts.js
+++ src/content-scripts/youtube.com/removes-youtube-shorts.js
@@ -1,87 +1,74 @@
 import * as browser from "webextension-polyfill";
 
+const scriptName = "removes-youtube-shorts";
+
+/* boilerplate */
+const prefix = `[control-center/${scriptName}]`;
+
 const logger = {
-	debug: (...msg) => {
-		console.debug("[control-center: removes-youtube-shorts]", ...msg);
-	},
-	info: (...msg) => {
-		console.info("[control-center: removes-youtube-shorts]", ...msg);
-	},
-	error: (err, when) => {
-		console.error(
-			"[control-center: removes-youtube-shorts]",
-			err,
-			"when",
-			when,
-		);
-	},
+  debug: (...msg) => console.debug(prefix, ...msg),
+  info: (...msg) => console.info(prefix, ...msg),
+  error: (err, why) => console.error(prefix, err, why),
 };
 
 logger.debug("loaded");
 
 async function getOption(key, defaultVal) {
-	try {
-		const item = await browser.storage.local.get(key);
-		if (item?.[key]) {
-			return item[key];
-		}
-		await browser.storage.local.set({ [key]: defaultVal });
-		return defaultVal;
-	} catch (err) {
-		logger.error(err, "setting/getting item from extension local storage");
-	}
+  try {
+    const item = await browser.storage.local.get(key);
+    if (item?.[key]) {
+      return item[key];
+    }
+    await browser.storage.local.set({ [key]: defaultVal });
+    return defaultVal;
+  } catch (err) {
+    logger.error(err, "setting/getting item from extension local storage");
+  }
 }
 
-const shouldRemoveYTShorts = await getOption(
-	"scripts.youtube-shorts.remove",
-	true,
-);
+/* boilerplate finish*/
 
-const defaultSelectors = [
-	"ytd-rich-section-renderer div#content", // as of Nov 3, 2024 [documented here](https://github.com/user-attachments/assets/1325c389-9dec-474e-a13c-45161f1ef8a1)
+const [optRemoveYTShorts, optCustomYTShortsSelectors] = await Promise.all([
+  getOption("scripts.youtube-shorts.remove", true),
+  getOption("scripts.youtube-shorts.css-selectors", []),
+]);
+
+const ytShortsSelectors = [
+  "ytd-rich-section-renderer div#content", //[refer here](https://github.com/user-attachments/assets/1325c389-9dec-474e-a13c-45161f1ef8a1)
+  "ytd-reel-shelf-renderer",
+  ...optCustomYTShortsSelectors,
 ];
 
-const userYTShortsSelectors = await getOption(
-	"scripts.youtube-shorts.css-selectors",
-	[],
-);
-
-const ytShortsSelectors = [...defaultSelectors, ...userYTShortsSelectors];
-
-if (shouldRemoveYTShorts) {
-	logger.info(
-		`should remove youtube shorts with selectors: ${ytShortsSelectors}`,
-	);
-}
-
 function observeAndAct(action) {
-	const observer = new MutationObserver(() => {
-		action();
-	});
+  const observer = new MutationObserver(() => {
+    action();
+  });
 
-	observer.observe(document.body, {
-		subtree: true,
-		childList: true,
-	});
+  observer.observe(document.body, {
+    subtree: true,
+    childList: true,
+  });
 
-	return observer;
+  return observer;
 }
 
 function removeFromDOM(selector) {
-	// FIXME: we are selecting all the elements again, and again, even though we have filtered it
-	const items = document.querySelectorAll(selector);
-	if (items.length > 0) {
-		logger.debug("found these youtube shorts, removing them", items);
-		items.forEach((item) => {
-			item.remove();
-		});
-	}
+  // FIXME: we are selecting all the elements again, and again, even though we have filtered it
+  const items = document.querySelectorAll(selector);
+  if (items.length > 0) {
+    logger.debug("found these youtube shorts, removing them", items);
+    for (const item of items) {
+      item.remove();
+    }
+  }
 }
 
-observeAndAct(() => {
-	ytShortsSelectors.forEach((selector) => {
-		if (selector) {
-			removeFromDOM(selector);
-		}
-	});
-});
+if (optRemoveYTShorts) {
+  logger.info(
+    `should remove youtube shorts with selectors: ${ytShortsSelectors}`,
+  );
+
+  observeAndAct(() => {
+    ytShortsSelectors.forEach(removeFromDOM);
+  });
+}
